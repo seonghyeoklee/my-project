52530c64dbaa3ed5c9ac02a50b81aff5efa4451e
shlee <46282970+seonghyeoklee@users.noreply.github.com>
2021-02-23 01:26:39 +0900

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,18 +1,55 @@
-from flask import Flask, jsonify
+from flask import Flask, current_app, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from conf import DB_CONNECT
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_pybo():
-    return jsonify(status = 'success', result="Hello Flask!!!")
 
-@app.route('/api')
-def apiTest():
-    result = {
-        'name': "nameTest",
-        'desc': "desc"
-    }
-    
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{DB_CONNECT['username']}:{DB_CONNECT['password']}@{DB_CONNECT['server']}:3306/{DB_CONNECT['dbname']}?charset=utf8mb4"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy() 
+db.init_app(app)
+
+
+def init_db():
+    db.init_app(current_app)
+    db.drop_all()
+    db.create_all()
+
+    sample_user = User(
+            username="lana", email="lana@lana.com", password=generate_password_hash("lanalana", method="sha256")
+    )
+    db.session.add(sample_user)
+    db.session.commit()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80))
+
+
+@app.route('/login')
+def login():
+    result = []
+    users = User.query.all()
+
+    for user in users:
+        print('========================')
+        print('id : ',user.id)
+        print('username: ',user.username)
+        print('email : ',user.email)
+        print('========================')
+        result.append(
+                {
+                    'id':user.id,
+                    'username':user.username,
+                    'email':user.email
+                }
+            )
+
     return jsonify(status = 'success', result=result)
